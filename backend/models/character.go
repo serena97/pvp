5437855d051158f46d98973424e866bc09cd8bbf
Eli <elijudah@gmail.com>
2021-02-21 00:06:58 +0000

    Add pvp statistics to char model

--- backend/models/character.go
+++ backend/models/character.go
@@ -1,21 +1,27 @@
 package models
 
+const (
+	Highest2v2PersonalRating int = 370
+	Highest3v3PersonalRating int = 595
+)
+
 // Character represents a WoW character to be loaded
 type Character struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Realm       string `json:"realm"`
-	Faction     string `json:"faction"`
-	Class       string `json:"class"`
-	Race        string `json:"race"`
-	Gender      string `json:"gender"`
-	Level       int    `json:"level"`
-	Guild       string `json:"guild"`
-	Spec        string `json:"spec"`
-	ItemLevel   int    `json:"item_level"`
-	Covenant    string `json:"covenant"`
-	RenownLevel int    `json:"renown_level"`
-	Media       Media
+	ID            int           `json:"id"`
+	Name          string        `json:"name"`
+	Realm         string        `json:"realm"`
+	Faction       string        `json:"faction"`
+	Class         string        `json:"class"`
+	Race          string        `json:"race"`
+	Gender        string        `json:"gender"`
+	Level         int           `json:"level"`
+	Guild         string        `json:"guild"`
+	Spec          string        `json:"spec"`
+	ItemLevel     int           `json:"item_level"`
+	Covenant      string        `json:"covenant"`
+	RenownLevel   int           `json:"renown_level"`
+	Media         Media         `json:"media"`
+	PvPStatistics PvPStatistics `json:"pvp_statistcs"`
 }
 
 // Media contains character media such as avatar and render links
@@ -33,4 +39,16 @@ type Covenent struct {
 		} `json:"chosen_covenant"`
 		RenownLevel int `json:"renown_level"`
 	} `json:"covenant_progress"`
+}
+
+type PvPStatistics struct {
+	TwoVTwo     Rating `json:"2v2"`
+	ThreeVThree Rating `json:"3v3"`
+	RBG         Rating `json:"rbg"`
+}
+
+type Rating struct {
+	HighestRating       float64 `json:"highest_rating"`
+	CurrentRating       float64 `json:"current_rating"`
+	SeasonHighestRating float64 `json:"season_highest_rating"`
 }
